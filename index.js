0ad43d195a5315d53c7036d91abb16a6ae4d4da7
Sathish <seesathish@gmail.com>
2018-07-31 00:14:05 +0530

    Prod Preparation

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
+
 
 // setting enviromental variable - $env:PORT=5000
 const port = process.env.PORT || 3000;
